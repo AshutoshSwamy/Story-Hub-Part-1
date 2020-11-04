72d7b512b5a3b6e971601d75863c727def448b9a
Ashutosh swamy <68917002+AshutoshSwamy@users.noreply.github.com>
2020-11-04 13:51:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import WriteStoryScreen from './Screens/WriteStoryScreen';
+import ReadStoryScreen from './Screens/ReadStoryScreen';
+
 
 export default function App() {
   return (
---
+++ ReadStoryScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {StyleSheet,Text,View} from 'react-native';
+
+export default class ReadStoryScreen extends React.Component
+render(){
+    return(
+        <View style={styles.container}>
+        <Text>Read Story</Text>
+        </View>
+    );
+}
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
